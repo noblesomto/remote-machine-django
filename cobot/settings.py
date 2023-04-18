951b6a281d994ef1e63cb8b3bce248e1063dc63f
Onyeibo Oku <onyeibo@schemefusion.com>
2023-04-18 18:29:58 +0100

    Adapt settings for collectstatic (deployment)

--- cobot/settings.py
+++ cobot/settings.py
@@ -129,17 +129,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+# Where 'collectstatic' will put media/static files
+# This is for deployment
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'html', 'static')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'html', 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    'static/',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
